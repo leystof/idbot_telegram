3c0a84a033f26f0718a3bd494f0845fa9641acfe
Leystof <allosmb554@gmail.com>
2021-04-09 01:32:06 +0300

    add  🪐file_unique_id

--- index.js
+++ index.js
@@ -1,76 +1,106 @@
-const Telegraf = require('telegraf');
+const Telegraf = require("telegraf");
 
-let TOKEN = "@BotFather"
+let TOKEN = "t.me/BotFather";
 const bot = new Telegraf(TOKEN);
 
-bot.catch((err, ctx) => {
+bot.catch((err) => {
   if (err.error_code === 403) {
-    console.log("bot was blocked by the user - skip")
+    console.log("bot was blocked by the user - skip");
   } else {
-    console.log(err)
+    console.log(err);
   }
-})
+});
 
-bot.command('start', async (ctx) => {
-  await ctx.reply("🤝 Добро пожаловать")
-  return ctx.replyWithSticker("CAACAgIAAxkBAAIW6F8sO6CKYDgyO7nQrOIW11PBH3hNAAJ0AAM7YCQUs8te1W3kR_QaBA")
-})
+bot.command("start", async (ctx) => {
+  await ctx.reply("🤝 Добро пожаловать");
+  return ctx.replyWithSticker(
+    "CAACAgIAAxkBAAIW6F8sO6CKYDgyO7nQrOIW11PBH3hNAAJ0AAM7YCQUs8te1W3kR_QaBA"
+  );
+});
 
-bot.command('help', async ({ reply, replyWithSticker }) => {
-  await reply(`
+bot.command("help", async ({ reply, replyWithSticker }) => {
+  await reply(
+    `
     📚 Для того чтобы вложения были доступны вашему боту: 
     1) Отправляете вложение своему боту.
     2) Пересыл сообщения с вложением сюда.
-    `.replace(/ {16}/g, ""));
-  return replyWithSticker("CAACAgUAAxkBAAI7bV-V92efOgf_86-ac4z21So0C0aEAAIgAAPg6vAdDBEzrevKwoAbBA")
-})
+    `.replace(/ {16}/g, "")
+  );
+  return replyWithSticker(
+    "CAACAgUAAxkBAAI7bV-V92efOgf_86-ac4z21So0C0aEAAIgAAPg6vAdDBEzrevKwoAbBA"
+  );
+});
 
-bot.on('photo', async (ctx) => {
-  let file_id = ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id
+bot.on("photo", async (ctx) => {
+  let file_id =
+    ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id;
+  let file_unique_id =
+    ctx.update.message.photo[ctx.update.message.photo.length - 1]
+      .file_unique_id;
 
-  return ctx.replyWithPhoto(file_id, {caption: "📃 file_id: " + file_id})
-})
+  return ctx.replyWithPhoto(file_id, {
+    caption: `📃 file_id: ${file_id} \n\n 🪐 file_unique_id: ${file_unique_id}`,
+  });
+});
 
-bot.on('sticker', async (ctx) => {
-  let file_id = ctx.update.message.sticker.file_id
+bot.on("sticker", async (ctx) => {
+  let file_id = ctx.update.message.sticker.file_id;
+  let file_unique_id = ctx.update.message.sticker.file_unique_id;
 
-  await ctx.replyWithSticker(file_id)
-  return ctx.reply("📃 file_id: " + file_id)
-})
+  await ctx.replyWithSticker(file_id);
+  return ctx.reply(
+    `📃 file_id: ${file_id} \n\n 🪐 file_unique_id: ${file_unique_id}`
+  );
+});
 
-bot.on('voice', async (ctx) => {
-  let file_id = ctx.update.message.voice.file_id
+bot.on("voice", async (ctx) => {
+  let file_id = ctx.update.message.voice.file_id;
+  let file_unique_id = ctx.update.message.voice.file_unique_id;
 
-  return ctx.replyWithVoice(file_id, {caption: "📃 file_id: " + file_id})
-})
+  return ctx.replyWithVoice(file_id, {
+    caption: `📃 file_id: ${file_id} \n\n 🪐 file_unique_id: ${file_unique_id}`,
+  });
+});
 
-bot.on('gif', async (ctx) => {
-  let file_id = ctx.update.message.gif.file_id
+bot.on("gif", async (ctx) => {
+  let file_id = ctx.update.message.gif.file_id;
+  let file_unique_id = ctx.update.message.gif.file_unique_id;
 
-  return ctx.replyWithGif(file_id, {caption: "📃 file_id: " + file_id})
-})
+  return ctx.replyWithGif(file_id, {
+    caption: `📃 file_id: ${file_id} \n\n 🪐 file_unique_id: ${file_unique_id}`,
+  });
+});
 
-bot.on('audio', async (ctx) => {
-  let file_id = ctx.update.message.audio.file_id
+bot.on("audio", async (ctx) => {
+  let file_id = ctx.update.message.audio.file_id;
+  let file_unique_id = ctx.update.message.audio.file_unique_id;
 
-  return ctx.replyWithAudio(file_id, {caption: "📃 file_id: " + file_id})
-})
+  return ctx.replyWithAudio(file_id, {
+    caption: `📃 file_id: ${file_id} \n\n 🪐 file_unique_id: ${file_unique_id}`,
+  });
+});
 
-bot.on('video', async (ctx) => {
-  let file_id = ctx.update.message.video.file_id
+bot.on("video", async (ctx) => {
+  let file_id = ctx.update.message.video.file_id;
+  let file_unique_id = ctx.update.message.video.file_unique_id;
 
-  return ctx.replyWithVideo(file_id, {caption: "📃 file_id: " + file_id})
-})
+  return ctx.replyWithVideo(file_id, {
+    caption: `📃 file_id: ${file_id} \n\n 🪐 file_unique_id: ${file_unique_id}`,
+  });
+});
 
-bot.on('document', async (ctx) => {
-  let file_id = ctx.update.message.document.file_id
+bot.on("document", async (ctx) => {
+  let file_id = ctx.update.message.document.file_id;
+  let file_unique_id = ctx.update.message.document.file_unique_id;
 
-  return ctx.replyWithDocument(file_id, {caption: "📃 file_id: " + file_id})
-})
+  return ctx.replyWithDocument(file_id, {
+    caption: `📃 file_id: ${file_id} \n\n 🪐 file_unique_id: ${file_unique_id}`,
+  });
+});
 
-bot.on('message', async (ctx) => {
-
-  return ctx.reply(`
+bot.on("message", async (ctx) => {
+  return ctx.reply(
+    `
   🔸 Для того чтобы получить file_id, отправьте:
   - 🙂 Стикер
   - 🌄 Фото
@@ -80,8 +110,8 @@ bot.on('message', async (ctx) => {
   - 🗣 Голосовое сообщение
   - 🗂 Документ
   
-  - 📝 Гайд по вложениям /help`.replace(/ {16}/g, ""))
-})
+  - 📝 Гайд по вложениям /help`.replace(/ {16}/g, "")
+  );
+});
 
-
-bot.launch()
+bot.launch();
